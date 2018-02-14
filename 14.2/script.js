20541397d6cc66c16d0099bd1e4b9d065882a590
Przemek Jurkiewicz <hankusekbb@gmail.com>
2018-02-14 19:29:03 +0100

    add pictures in script.js

--- 14.2/script.js
+++ 14.2/script.js
@@ -3,22 +3,25 @@ var movies = [
         id: 1,
         title: 'Harry Potter',
         desc: 'film o czarodzieju',
-        pict: '<img src="https://cdn4.dualshockers.com/wp-content/uploads/2017/11/Harry-Potter.jpg">'
+        pict: './plakat/Harry.jpg'
     },
     {
         id: 2,
         title: 'Król Lew',
-        desc: 'Film o królu sawanny'
+        desc: 'Film o królu sawanny',
+        pict: './plakat/krol.jpg'
     },
     {
     	id: 3,
     	title: 'Riddick',
-    	desc: 'Furianin na przeklętej planecie'
+    	desc: 'Furianin na przeklętej planecie',
+    	pict: './plakat/Riddick.jpg'
     },
     {
     	id: 4,
     	title: 'Terra Nova',
-    	desc: 'Serial o śmiałkach chcących zbudować cywilizacje od nowa'
+    	desc: 'Serial o śmiałkach chcących zbudować cywilizacje od nowa',
+    	pict: './plakat/Terra.jpg'
     }
 ];
 
@@ -26,7 +29,7 @@ var moviesElements = movies.map(function(movie) {
     return React.createElement('li', {key: movie.id},
         React.createElement('h2', {}, movie.title),
         React.createElement('p', {}, movie.desc),
-        React.createElement('p', {}, movie.pict)
+        React.createElement('img', {src: movie.pict})
     );
 });
 
